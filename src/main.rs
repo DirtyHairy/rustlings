19325d83e2a9bc046eb2e8e282c418c916d91d00
Christian Speckner <christian.speckner@mayflower.de>
2022-12-23 10:28:56 +0100

    Refactor subcommands.

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,48 @@ mod file;
 mod sdl_display;
 
 use anyhow::Result;
-use clap::{Arg, Command};
+use clap::{Arg, ArgMatches, Command};
 
 use std::path::Path;
 
+const ARG_GAME_DATA_PATH: &str = "GAME_DATA";
+
+fn game_data_path(matches: &ArgMatches) -> &Path {
+    Path::new(
+        matches
+            .get_one::<String>(ARG_GAME_DATA_PATH)
+            .expect("unreachable"),
+    )
+}
+
 fn main() -> Result<()> {
+    let arg_data_path = Arg::new(ARG_GAME_DATA_PATH)
+        .required(true)
+        .help("path to lemmings data files")
+        .index(1);
+
     let mut command = Command::new("rustlings")
         .about("rust by rodent")
-        .arg(
-            Arg::new("PATH")
-                .required(true)
-                .help("path to .dat files")
-                .index(1),
+        .subcommand(
+            Command::new("sprites")
+                .about("display lemming sprites")
+                .arg(arg_data_path.clone()),
         )
-        .subcommand(Command::new("sprites").about("display lemming sprites"))
-        .subcommand(Command::new("tilesets").about("display tilesets"));
+        .subcommand(
+            Command::new("tilesets")
+                .about("display tilesets")
+                .arg(arg_data_path.clone()),
+        );
 
     let matches = command.clone().get_matches();
 
-    let path = Path::new(matches.get_one::<String>("PATH").expect("unreachable"));
-
     match matches.subcommand() {
-        Some(("sprites", _)) => cmd::sprites::main(path),
-        Some(("tilesets", _)) => cmd::tilesets::main(path),
+        Some(("sprites", subcommand_matches)) => {
+            cmd::sprites::main(game_data_path(subcommand_matches))
+        }
+        Some(("tilesets", subcommand_matches)) => {
+            cmd::tilesets::main(game_data_path(subcommand_matches))
+        }
         _ => drop(command.print_help()),
     }
 
