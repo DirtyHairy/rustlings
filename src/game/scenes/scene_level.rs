1c695360d670df63e85f3219377b1ec3d14f7a17
Christian Speckner <christian.speckner@mayflower.de>
2026-02-24 22:16:40 +0100

    Render minimap.

--- src/game/scenes/scene_level.rs
+++ src/game/scenes/scene_level.rs
@@ -23,6 +23,14 @@ const SCREEN_HEIGHT: usize = 200;
 
 const SKILL_PANEL_HEIGHT: usize = 40;
 
+const MINIMAP_HEIGHT: usize = 18;
+const MINIMAP_WIDTH: usize = 100;
+const MINIMAP_RIGHT: usize = 8;
+const MINIMAP_BOTTOM: usize = 3;
+
+const TEXTURE_ID_MAIN_SCREEN: usize = 0;
+const TEXTURE_ID_PREVIEW: usize = 1;
+
 pub struct SceneLevel<'texture_creator> {
     game_data: Rc<GameData>,
     game_state: GameState,
@@ -30,7 +38,6 @@ pub struct SceneLevel<'texture_creator> {
 
     level: Level,
     terrain: Bitmap,
-    palette: [PaletteEntry; PALETTE_SIZE],
 
     texture_terrain: Texture<'texture_creator>,
     texture_skill_panel: Texture<'texture_creator>,
@@ -39,8 +46,6 @@ pub struct SceneLevel<'texture_creator> {
     texture_screen: Texture<'texture_creator>,
 }
 
-const TEXTURE_ID_MAIN_SCREEN: usize = 0;
-
 impl<'texture_creator> SceneLevel<'texture_creator> {
     pub fn new<T>(
         game_data: Rc<GameData>,
@@ -48,7 +53,7 @@ impl<'texture_creator> SceneLevel<'texture_creator> {
         scene_state: SceneState,
         texture_creator: &'texture_creator TextureCreator<T>,
     ) -> Result<Self> {
-        let level = game_data.resolve_level(0)?;
+        let level = game_data.resolve_level(21)?;
         let terrain = game_data.compose_terrain(&level)?;
         let palette = game_data.resolve_palette(&level)?;
 
@@ -60,12 +65,11 @@ impl<'texture_creator> SceneLevel<'texture_creator> {
             texture_creator,
         )?;
 
-        let mut texture_level = texture_creator.create_texture_target(
+        let texture_level = texture_creator.create_texture_target(
             PixelFormat::RGBA8888,
             LEVEL_WIDTH as u32,
             LEVEL_HEIGHT as u32,
         )?;
-        texture_level.set_scale_mode(ScaleMode::Nearest);
 
         let texture_screen = texture_creator.create_texture_target(
             PixelFormat::RGBA8888,
@@ -87,7 +91,6 @@ impl<'texture_creator> SceneLevel<'texture_creator> {
             state,
             level,
             terrain,
-            palette,
             texture_terrain,
             texture_skill_panel,
             texture_level,
@@ -115,10 +118,22 @@ impl<'texture_creator> Scene<'texture_creator> for SceneLevel<'texture_creator> 
 
     fn register_layers(&self, compositor: &mut dyn crate::scene::Compositor) {
         compositor.add_layer(
-            TEXTURE_ID_MAIN_SCREEN as usize,
+            TEXTURE_ID_MAIN_SCREEN,
             320,
             200,
             Rect::new(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT),
+        );
+
+        compositor.add_layer(
+            TEXTURE_ID_PREVIEW,
+            LEVEL_WIDTH,
+            LEVEL_HEIGHT,
+            Rect::new(
+                SCREEN_WIDTH - MINIMAP_RIGHT - MINIMAP_WIDTH,
+                SCREEN_HEIGHT - MINIMAP_BOTTOM - MINIMAP_HEIGHT,
+                MINIMAP_WIDTH,
+                MINIMAP_HEIGHT,
+            ),
         );
     }
 
@@ -141,6 +156,7 @@ impl<'texture_creator> Scene<'texture_creator> for SceneLevel<'texture_creator> 
                 ),
             )?;
 
+            self.texture_level.set_scale_mode(ScaleMode::Nearest);
             canvas.copy(
                 &self.texture_level,
                 SdlRect::new(
@@ -161,6 +177,7 @@ impl<'texture_creator> Scene<'texture_creator> for SceneLevel<'texture_creator> 
     fn texture(&mut self, id: usize) -> Result<&mut Texture<'texture_creator>> {
         match id {
             TEXTURE_ID_MAIN_SCREEN => Ok(&mut self.texture_screen),
+            TEXTURE_ID_PREVIEW => Ok(&mut self.texture_level),
             _ => Err(anyhow::format_err!("invalid texture id {}", id)),
         }
     }
